a214607cef8076d96629638a1bd415fb7fcac453
Tajeshwar Khara <tkhara@gmail.com>
2018-12-10 16:59:25 +0530

    Databse credentials video done.
    https://www.linkedin.com/learning/php-object-oriented-programming-with-databases/database-credentials?autoplay=true
    
    Added the db_credentials file with the database credentials and added this file into initialize.php

--- Ex_Files_PHP_OOP_Databases/Chapter_01/01_03/insert_bicycles.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO bicycles (brand, model, year, category, gender, color, price, weight_kg, condition_id, description) VALUES ('Trek','Emonda','2017','Hybrid','Unisex','black','1495.00','1.5','5','');
-
-INSERT INTO bicycles (brand, model, year, category, gender, color, price, weight_kg, condition_id, description) VALUES ('Cannondale','Synapse','2016','Road','Unisex','matte black','1999.00','1.0','5','');
--- Ex_Files_PHP_OOP_Databases/Chapter_05/05_06/chain_gang/public/staff/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once('../../private/initialize.php');
-
-// Log out the admin
-
-redirect_to(url_for('/staff/login.php'));
-
-?>
